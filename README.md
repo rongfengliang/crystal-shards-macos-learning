bfe4bb6b8f5d5f1f909f17609d6d96132073e377
rong <1141591465@qq.com>
2020-07-02 21:39:02 +0800

    crystal mac os learning

---
+++ README.md
@@ -0,0 +1,6 @@
+# crystal && shards mac learning
+## cmd
+
+```code
+shards build  --link-flags -L/usr/local/opt/openssl@1.1/lib (replace with your openssl lib)
+```
---
+++ src/main.cr
@@ -0,0 +1,10 @@
+# A very basic HTTP server
+require "http/server"
+
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello world, got #{context.request.path}!"
+end
+
+puts "Listening on http://127.0.0.1:9080"
+server.listen("0.0.0.0",9080)
